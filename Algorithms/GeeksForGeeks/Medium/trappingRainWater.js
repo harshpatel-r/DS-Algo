644293f5b6f1a009508b374ca7098d86d5cabda4
Harsh Patel <harshpatel23900@gmail.com>
2022-09-19 21:49:07 +0530

    GFG | medium | Trapping Rain Water

--- Algorithms/GeeksForGeeks/Medium/trappingRainWater.js
+++ Algorithms/GeeksForGeeks/Medium/trappingRainWater.js
@@ -1,7 +1,11 @@
 
 //# Trapping Rain Water
 
-// Given an array arr[] of N non-negative integers representing the height of blocks. If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+/*
+Given an array arr[] of N non-negative integers representing the height of blocks. 
+If width of each block is 1, compute how much water can be trapped between the 
+blocks during the rainy season. 
+*/
 
 // SOLUTION 
 
