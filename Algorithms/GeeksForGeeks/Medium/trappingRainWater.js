f0e8dfab8c1d09b66f4bed00c1cb90a864478746
Harsh Patel <harshpatel23900@gmail.com>
2022-09-19 21:33:06 +0530

    GFG | medium | Trapping Rain Water

--- Algorithms/GeeksForGeeks/Medium/trappingRainWater.js
+++ Algorithms/GeeksForGeeks/Medium/trappingRainWater.js
@@ -19,11 +19,13 @@ class Solution {
         let left = new Array(n);
         let right = new Array(n);
         left[0] = arr[0];
+
         for (let i = 1; i < n; i++) {
             left[i] = Math.max(left[i - 1], arr[i])
         }
 
         right[n - 1] = arr[n - 1];
+        
         for (let i = n - 2; i >= 0; i--) {
             right[i] = Math.max(right[i + 1], arr[i])
         }
