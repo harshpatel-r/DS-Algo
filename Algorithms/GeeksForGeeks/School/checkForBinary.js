8c3a4b3af0f01c6385cc5de3db747720883bb4f8
Harsh Patel <harshpatel23900@gmail.com>
2022-09-19 21:10:46 +0530

    GFG | school | checkForBinary

---
+++ Algorithms/GeeksForGeeks/School/checkForBinary.js
@@ -0,0 +1,28 @@
+
+//# CHECK FOR BINARY 
+
+// Given a non-empty sequence of characters str, return true if sequence is Binary, else return false.
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {string} str
+ * @returns {boolean}
+*/
+
+class Solution{
+    isBinary(str){
+        //code here
+str = str.toString()
+let isbinary = false;
+if(str.length!=1){
+ if(str.includes("0") && str.includes("1")) isbinary = true;
+}
+else{
+if(str.includes("0") || str.includes("1")) isbinary = true;
+}
+return isbinary
+    }
+}
