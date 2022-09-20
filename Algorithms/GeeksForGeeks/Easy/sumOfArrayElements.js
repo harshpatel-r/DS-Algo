fe12396176775c6ddc6fd26cfeac66824bda7559
Harsh Patel <harshpatel23900@gmail.com>
2022-09-20 18:55:14 +0530

    GFG | easy | Sum of Array Elements

---
+++ Algorithms/GeeksForGeeks/Easy/sumOfArrayElements.js
@@ -0,0 +1,14 @@
+
+//# Sum of Array Elements
+
+// Given an integer array arr of size n, you need to sum the elements of arr.
+
+// SOLUTION
+
+//User function Template for javascript
+
+class Solution {
+    sumElement(arr, n) {
+        return arr.reduce((a, b) => a + b);
+    }
+}
