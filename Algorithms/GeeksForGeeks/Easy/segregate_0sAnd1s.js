30f91ef63d7cf05657beb53b2be7e66933f37b9d
Harsh Patel <harshpatel23900@gmail.com>
2022-09-20 19:31:34 +0530

    GFG | easy | Segregate 0s and 1s

---
+++ Algorithms/GeeksForGeeks/Easy/segregate_0sAnd1s.js
@@ -0,0 +1,23 @@
+
+//# Segregate 0s and 1s
+
+/*
+Given an array of length N consisting of only 0s and 1s in random order. 
+Modify the array to segregate 0s on left side and 1s on the right side of 
+the array. 
+*/
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+*/
+
+class Solution {
+    segregate0and1(arr, n) {
+        arr.sort()
+    }
+}
