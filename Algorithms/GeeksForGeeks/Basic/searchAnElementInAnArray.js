70c67f9799ad463efe248ebfcf9c104985b84e96
Harsh Patel <harshpatel23900@gmail.com>
2022-09-21 19:17:49 +0530

    GFG | basic | Search an Element in an array

---
+++ Algorithms/GeeksForGeeks/Basic/searchAnElementInAnArray.js
@@ -0,0 +1,24 @@
+
+//# Search an Element in an array
+
+/*
+Given an integer array and another integer element. The task is to 
+find if the given element is present in array or not.
+*/
+
+// SOLUTION 
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} N
+ * @param {number} X
+ * @return {number}
+ */
+class Solution {
+    search(arr, N, X) {
+        // write your code here
+        return arr.indexOf(X);
+    }
+}
