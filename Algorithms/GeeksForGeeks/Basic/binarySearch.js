d651b9c93869dc13863a00d134f2c2c0408b9666
Harsh Patel <harshpatel23900@gmail.com>
2022-09-21 18:55:49 +0530

    GFG | basic | Binary Search

---
+++ Algorithms/GeeksForGeeks/Basic/binarySearch.js
@@ -0,0 +1,25 @@
+
+//# Binary Search
+
+/*
+Given a sorted array of size N and an integer K, find the position at which K 
+is present in the array using binary search.
+*/
+
+// SOLUTION
+
+// User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @param {number} k
+ * @returns {number}
+*/
+
+class Solution {
+    binarysearch(arr, n, k) {
+        // code here
+        return arr.indexOf(k)
+    }
+}
