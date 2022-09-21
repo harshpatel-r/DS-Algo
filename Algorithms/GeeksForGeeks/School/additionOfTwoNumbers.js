491dccf7a8c8a1d29b0b7435f020eb7374a8626f
Harsh Patel <harshpatel23900@gmail.com>
2022-09-21 16:51:45 +0530

    GFG | school | Addition of Two Numbers

---
+++ Algorithms/GeeksForGeeks/School/additionOfTwoNumbers.js
@@ -0,0 +1,21 @@
+
+//# Addition of Two Numbers
+
+// Given two numbers A and B. Your task is to return the sum of A and B.
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {number} A
+ * @param {number} B
+ * @return {number}
+*/
+
+class Solution {
+    addition(A, B) {
+        //code here
+        return A + B;
+    }
+}
