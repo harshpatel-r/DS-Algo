b553b49e3c2ddefe2c55e6a5e2fefaeae2c478c2
Harsh Patel <harshpatel23900@gmail.com>
2022-09-21 16:59:57 +0530

    GFG | school | Remainder Evaluation

---
+++ Algorithms/GeeksForGeeks/School/remainderEvaluation.js
@@ -0,0 +1,24 @@
+
+//# Remainder Evaluation
+
+/*
+Given two positive integers num1 and num2, the task is to find the remainder 
+when num1 is divided by num2.
+*/
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number}
+*/
+
+class Solution{
+    findRemainder(num1, num2){
+        //code here
+        return num1 % num2;
+    }
+}
