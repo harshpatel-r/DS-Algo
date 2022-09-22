784686679b852b803a12fc4ec909cc30c3843ec1
Harsh Patel <harshpatel23900@gmail.com>
2022-09-22 18:10:28 +0530

    GFG | basic | Largest Element in Array

---
+++ Algorithms/GeeksForGeeks/Basic/largestElementInArray.js
@@ -0,0 +1,15 @@
+
+//# Largest Element in Array
+
+// Given an array A[] of size n. The task is to find the largest element in it.
+
+// SOLUTION
+
+//User function Template for javascript
+
+class Solution {
+    largest(arr, n) {
+        // write your code here
+        return Math.max(...arr);
+    }
+}
