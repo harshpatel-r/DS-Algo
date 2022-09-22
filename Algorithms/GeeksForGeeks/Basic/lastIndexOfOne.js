2428c5d91fe68f8765af4f661cfdb0f24b7bd70d
Harsh Patel <harshpatel23900@gmail.com>
2022-09-22 18:08:29 +0530

    GFG | basic | Last index of One

---
+++ Algorithms/GeeksForGeeks/Basic/lastIndexOfOne.js
@@ -0,0 +1,19 @@
+
+//# Last index of One
+
+// Given a string S consisting only '0's and '1's,  find the last index of the '1' present in it.
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {string} S
+ * @returns {number}
+*/
+
+class Solution {
+    lastIndex(S) {
+        return S.lastIndexOf(1);
+    }
+}
