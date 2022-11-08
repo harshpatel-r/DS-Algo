84bf28ff9b3f16e18335273f3b7b76bba2a6657e
Harsh Patel <harshpatel23900@gmail.com>
2022-11-08 13:06:52 +0530

    Algorithms/GeeksForGeeks/Easy/non-RepeatingElement.js

---
+++ Algorithms/GeeksForGeeks/Easy/non-RepeatingElement.js
@@ -0,0 +1,10 @@
+let arr = [5, 2, -1, 3,4];
+let n = arr.length;
+
+// for(let i = 0 ; i < n ; i++){
+//     if(arr.indexOf(arr[i],i+1) == -1 && arr.lastIndexOf(arr[i],i-1) == -1){
+//         return arr[i];
+//     }
+// }
+
+
