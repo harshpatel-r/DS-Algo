0bd7e7cd9c11c0aeff21639baef671f5c5aac7d0
Harsh Patel <harshpatel23900@gmail.com>
2023-01-20 22:56:25 +0530

    GFG | basic | Start Coding

---
+++ Algorithms/GeeksForGeeks/Basic/startCoding.js
@@ -0,0 +1,20 @@
+
+//# Start Coding
+
+/*
+When learning a new language, we first learn to output some message. Here, 
+we'll start with the famous Hello World message. Now, here you are given a 
+function to complete. Don't worry about the ins and outs of functions, just 
+add the command (cout<<"Hello World") to print Hello World.
+*/
+
+// SOLUTION
+
+//User function Template for javascript
+
+class Solution {
+    utility() {
+        // write your if statement here
+        console.log("Hello World");
+    }
+}
