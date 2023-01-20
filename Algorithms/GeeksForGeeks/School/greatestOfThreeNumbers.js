ebd575103e97baceba4f27f8063d12bfcbddbc10
Harsh Patel <harshpatel23900@gmail.com>
2023-01-20 22:16:26 +0530

    GFG | school | Greatest of three numbers

---
+++ Algorithms/GeeksForGeeks/School/greatestOfThreeNumbers.js
@@ -0,0 +1,22 @@
+
+//# Greatest of three numbers
+
+// Given 3 numbers A, B and C. Find the greatest number among them.
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {number} A
+ * @param {number} B
+ * @param {number} C
+ * @returns {number}
+*/
+
+class Solution {
+    greatestOfThree(A, B, C) {
+        return Math.max(A, B, C);
+    }
+}
+
