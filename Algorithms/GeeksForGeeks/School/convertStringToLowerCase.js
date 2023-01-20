6bcfec25955c912b3872da06137f1285e02c489f
Harsh Patel <harshpatel23900@gmail.com>
2023-01-20 22:05:38 +0530

    GFG | school | Convert String to LowerCase

---
+++ Algorithms/GeeksForGeeks/School/convertStringToLowerCase.js
@@ -0,0 +1,20 @@
+
+//# Convert String to LowerCase
+
+// Given a string S. The task is to convert characters of string to lowercase.]
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {string} str
+ * @returns {string}
+*/
+
+class Solution {
+    toLower(arr, n) {
+        //code here
+        return arr.toLowerCase();
+    }
+}
