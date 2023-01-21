db5e49f79f890d42e9935d9fda2d59f2fb1687e8
Harsh Patel <harshpatel23900@gmail.com>
2023-01-21 12:30:20 +0530

    GFG | school | Odd or Even

---
+++ Algorithms/GeeksForGeeks/School/oddOrEven.js
@@ -0,0 +1,20 @@
+
+//# Odd or Even
+
+// Given a positive integer N, determine whether it is odd or even.
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {number} N
+ * @return {string}
+*/
+
+class Solution {
+    oddEven(N) {
+        //code here
+        return (N % 2 == 0) ? "even" : "odd";
+    }
+}
