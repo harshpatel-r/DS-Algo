64b16a3a000a48f1fba94db848c943ea8a336e8b
Harsh Patel <harshpatel23900@gmail.com>
2023-01-29 13:20:31 +0530

    GFG | basic | Index of first 1 in a sorted array of 0s and 1s

---
+++ Algorithms/GeeksForGeeks/Basic/index_of_first_1_in_a_sorted_array_of _0s_and_1s.js
@@ -0,0 +1,24 @@
+
+//# Index of first 1 in a sorted array of 0s and 1s
+
+/*
+Given a sorted array consisting 0s and 1s. The task is to find the index 
+of first 1 in the given array. 
+*/
+
+// SOLUTION
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @returns {number}
+*/
+
+class Solution {
+    firstIndex(arr, n) {
+        //code here
+        return arr.indexOf(1);
+    }
+}
