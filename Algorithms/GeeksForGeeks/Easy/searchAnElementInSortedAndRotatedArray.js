0e554a17ecbe05886c2c90b148a4e3d3c660abfd
Harsh Patel <harshpatel23900@gmail.com>
2023-02-09 22:00:00 +0530

    GFG | easy | Search an element in sorted and rotated array

---
+++ Algorithms/GeeksForGeeks/Easy/searchAnElementInSortedAndRotatedArray.js
@@ -0,0 +1,23 @@
+
+//# Search an element in sorted and rotated array
+
+/*
+Given a sorted and rotated array A of N distinct elements which are rotated at some point, and 
+given an element K. The task is to find the index of the given element K in array A.
+*/
+
+
+// SOLUTION
+
+//User function Template for javascript
+/**
+ * @param {number[]} arr
+ * @param {number} K
+ * @returns {number}
+ */
+class Solution {
+    Search(arr, K) {
+        //code here
+        return arr.indexOf(K);
+    }
+}
