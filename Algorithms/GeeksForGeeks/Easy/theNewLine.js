7fbc8ba4a9eb5a8a71c84e2eb58b41deb5b3e8a8
Harsh Patel <harshpatel23900@gmail.com>
2023-03-16 23:53:27 +0530

    GFG | easy | The New Line

---
+++ Algorithms/GeeksForGeeks/Easy/theNewLine.js
@@ -0,0 +1,21 @@
+
+//# The New Line
+
+/*
+You are familiar with producing output using C++/C. In this task, you'll be required to write three messages in 
+three separate lines. Write Geeks for Geeks. But wait a minute!! You need to write each word of it in a 
+separate line.
+*/
+
+// SOLUTION
+
+
+//User function Template for javascript
+class Solution {
+    utility() {
+        // write your if statement here
+        console.log("Geeks");
+        console.log("for");
+        console.log("Geeks");
+    }
+}
