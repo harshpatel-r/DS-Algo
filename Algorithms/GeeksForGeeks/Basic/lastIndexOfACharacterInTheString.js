cb04e1e2778dcfb007ab470197f9a70d54377eb8
Harsh Patel <harshpatel23900@gmail.com>
2023-05-02 12:15:12 +0530

    GFG | basic | Last index of a character in the string

---
+++ Algorithms/GeeksForGeeks/Basic/lastIndexOfACharacterInTheString.js
@@ -0,0 +1,24 @@
+
+// Last index of a character in the string
+
+/*
+Given a string S  and a character X, the task is to find the last index (0 based indexing) of X in string S. 
+If no index found then the answer will be -1.
+*/
+
+// SOLUTION
+
+
+//User function Template for javascript
+/**
+ * @param {string} s
+ * @param {char} p
+ * @returns {number}
+*/
+
+class Solution {
+    LastIndex(s, p) {
+        //code here
+        return s.lastIndexOf(p);
+    }
+}
