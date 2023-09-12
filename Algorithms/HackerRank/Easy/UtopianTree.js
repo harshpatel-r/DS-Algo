80f28facab18c63c6d18914c256f22edc9cd66c7
Harsh Patel <harshpatel23900@gmail.com>
2023-09-12 17:28:18 +0530

    HackerRank | Easy | Utopian Tree

---
+++ Algorithms/HackerRank/Easy/UtopianTree.js
@@ -0,0 +1,81 @@
+
+//# Utopian Tree
+
+/*
+--> https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
+
+The Utopian Tree goes through 2 cycles of growth every year. Each spring, it doubles in height. Each summer, its height increases by 1 meter.
+
+A Utopian Tree sapling with a height of 1 meter is planted at the onset of spring. How tall will the tree be after  growth cycles?
+
+For example, if the number of growth cycles is , the calculations are as follows:
+
+Period  Height
+0          1
+1          2
+2          3
+3          6
+4          7
+5          14
+*/
+
+// SOLUTION
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'utopianTree' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function utopianTree(n) {
+    // Write your code here
+    let res = 0;
+
+    for (let i = 0; i <= n; i++) {
+        res = i % 2 === 0 ? res += 1 : res * 2;
+    }
+
+    return res;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const result = utopianTree(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
