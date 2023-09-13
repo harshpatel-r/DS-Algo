229a42362a527468b14ecd1e245c25c4f88a83b5
Harsh Patel <harshpatel23900@gmail.com>
2023-09-13 11:32:37 +0530

    HackerRank | Easy | Find Digits

---
+++ Algorithms/HackerRank/Easy/FindDigits.js
@@ -0,0 +1,71 @@
+
+//# Find Digits
+
+/*
+Problem Statement :- 
+https://www.hackerrank.com/challenges/find-digits/problem?isFullScreen=true
+*/
+
+// SOLUTION
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function findDigits(n) {
+    // Write your code here 
+    let num = n.toString();
+    let res = 0;
+
+    for (let i = 0; i < num.length; i++) {
+        let d = parseInt(num[i]);
+        if (n % d === 0) res++;
+    }
+
+    return res;
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const result = findDigits(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
