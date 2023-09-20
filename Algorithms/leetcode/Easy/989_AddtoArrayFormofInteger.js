61595719897cdc65fb2505ca039043dff3dcd46d
Harsh Patel <harshpatel23900@gmail.com>
2023-09-20 12:19:31 +0530

    leetcode | easy | 989. Add to Array-Form of Integer

---
+++ Algorithms/leetcode/Easy/989_AddtoArrayFormofInteger.js
@@ -0,0 +1,24 @@
+
+//# 989. Add to Array-Form of Integer
+
+/*
+The array-form of an integer num is an array representing its digits in left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+*/
+
+// SOLUTION
+
+
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+
+var addToArrayForm = function (num, k) {
+    let num1 = BigInt(num.join(""));
+    let sum = num1 + BigInt(k);
+    return sum.toString().split("");
+};
