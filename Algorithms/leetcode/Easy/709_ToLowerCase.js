869e6bcfac8d1f8e0fb0b5b83bb7df2d9a68be64
Harsh Patel <harshpatel23900@gmail.com>
2023-12-16 23:58:51 +0530

    leetcode | easy | 709. To Lower Case

---
+++ Algorithms/leetcode/Easy/709_ToLowerCase.js
@@ -0,0 +1,16 @@
+
+//# 709. To Lower Case
+
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+
+// SOLUTION
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
