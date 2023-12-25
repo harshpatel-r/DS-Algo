e06395fd2a2af2571e9a1b4c82de4983dd77518f
Harsh Patel <harshpatel23900@gmail.com>
2023-12-25 16:16:30 +0530

    leetcode | easy | 1108. Defanging an IP Address

---
+++ Algorithms/leetcode/Easy/1108_DefanginganIPAddress.js
@@ -0,0 +1,20 @@
+
+//# 1108. Defanging an IP Address
+
+/*
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+*/
+
+
+// SOLUTION 
+
+/**
+ * @param {string} address
+ * @return {string}
+ */
+
+var defangIPaddr = function (address) {
+    return address.replace(/\./g, "[.]");
+};
