341f03d8b3cee645f475ccd4723c40297297968a
Harsh Patel <harshpatel23900@gmail.com>
2023-12-28 12:07:51 +0530

    leetcode | easy | 1148. Article Views I(Database)

---
+++ Algorithms/leetcode/Easy/1148_ArticleViewsI.sql
@@ -0,0 +1,18 @@
+
+--# 1148. Article Views I
+
+/*
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+The result format is in the following example.
+https://leetcode.com/problems/article-views-i/description/
+*/
+
+
+-- SOLUTION
+
+SELECT DISTINCT author_id AS id FROM Views
+WHERE author_id = viewer_id
+ORDER BY id;
