272913b4711bbed25d72b64bc4f07a0da0274953
Harsh Patel <harshpatel23900@gmail.com>
2024-01-18 17:22:02 +0530

    leetcode | easy | 1385. Find the Distance Value Between Two Arrays

---
+++ Algorithms/leetcode/Easy/1385_FindtheDistanceValueBetweenTwoArrays.js
@@ -0,0 +1,38 @@
+
+//# 1385. Find the Distance Value Between Two Arrays
+
+/*
+Given two integer arrays arr1 and arr2, and the integer d, return the distance value between the two arrays.
+
+The distance value is defined as the number of elements arr1[i] such that there is not any element arr2[j] 
+where |arr1[i]-arr2[j]| <= d.
+*/
+
+
+// SOLUTION
+
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @return {number}
+ */
+
+var findTheDistanceValue = function (arr1, arr2, d) {
+    let count = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+        let meetConditions = true;
+
+        for (let j = 0; j < arr2.length; j++) {
+            if (!(Math.abs(arr1[i] - arr2[j]) > d)) {
+                meetConditions = false;
+                break;
+            }
+        }
+
+        if (meetConditions) count++;
+    }
+
+    return count;
+};
