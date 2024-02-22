7155a832cbb5d626eb007bf756dd086be283c9d5
Harsh Patel <harshpatel23900@gmail.com>
2024-02-22 12:23:39 +0530

    leetcode | easy | 1217. Minimum Cost to Move Chips to The Same Position

---
+++ Algorithms/leetcode/Easy/1217_MinimumCosttoMoveChipstoTheSamePosition.js
@@ -0,0 +1,32 @@
+
+//# 1217. Minimum Cost to Move Chips to The Same Position
+
+/*
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+-> position[i] + 2 or position[i] - 2 with cost = 0.
+-> position[i] + 1 or position[i] - 1 with cost = 1.
+
+Return the minimum cost needed to move all the chips to the same position.
+*/
+
+
+// SOLUTION
+
+/**
+ * @param {number[]} position
+ * @return {number}
+ */
+
+var minCostToMoveChips = function (position) {
+    let odd = 0;
+    let even = 0;
+
+    for (let i = 0; i < position.length; i++) {
+        position[i] % 2 === 0 ? even++ : odd++;
+    }
+
+    return odd > even ? even : odd;
+};
