1c265aa802c798e0eb9bcadf7e3672b4892f66e2
Harsh Patel <harshpatel23900@gmail.com>
2025-01-08 12:36:13 +0530

    leetcode | easy | 3042. Count Prefix and Suffix Pairs I

---
+++ Algorithms/leetcode/Easy/3042_CountPrefixandSuffixPairsI.js
@@ -0,0 +1,48 @@
+
+//# 3042. Count Prefix and Suffix Pairs I
+
+/*
+You are given a 0-indexed string array words.
+
+Let's define a boolean function isPrefixAndSuffix that takes two strings, str1 and str2:
+
+isPrefixAndSuffix(str1, str2) returns true if str1 is both a prefix and a suffix of str2, 
+and false otherwise.
+
+For example, isPrefixAndSuffix("aba", "ababa") is true because "aba" is a prefix of 
+"ababa" and also a suffix, but isPrefixAndSuffix("abc", "abcd") is false.
+
+Return an integer denoting the number of index pairs (i, j) such that i < j, and 
+isPrefixAndSuffix(words[i], words[j]) is true.
+*/
+
+
+// SOLUTION:
+
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+
+var countPrefixSuffixPairs = function (words) {
+    let count = 0;
+    let n = words.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (isPrefixAndSuffix(words[i], words[j])) {
+                count++;
+            }
+        }
+    }
+
+    function isPrefixAndSuffix(str1, str2) {
+        if (str1.length > str2.length) {
+            return false;
+        }
+
+        return str2.startsWith(str1) && str2.endsWith(str1)
+    }
+
+    return count;
+};
